d853b385286164d25fe8483b013ddee59bed42d2
Max Rozdobudko <max.rozdobudko@gmail.com>
2019-10-21 12:58:49 +0300

    Implements showing a keyboard with default settings.

---
+++ floating-keyboard-ios/FloatingKeyboard/FloatingKeyboard/ANXTextField.h
@@ -0,0 +1,17 @@
+//
+//  ANXTextField.h
+//  ANXFloatingKeyboard
+//
+//  Created by Max Rozdobudko on 21.10.2019.
+//  Copyright © 2019 AirExt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ANXTextField : UITextField
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ floating-keyboard-ios/FloatingKeyboard/FloatingKeyboard/ANXTextField.m
@@ -0,0 +1,21 @@
+//
+//  ANXTextField.m
+//  ANXFloatingKeyboard
+//
+//  Created by Max Rozdobudko on 21.10.2019.
+//  Copyright © 2019 AirExt. All rights reserved.
+//
+
+#import "ANXTextField.h"
+
+@implementation ANXTextField
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
